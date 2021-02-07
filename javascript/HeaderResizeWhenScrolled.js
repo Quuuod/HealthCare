55e12f9613a54b9d3b8d667b08e5f2af205db154
QQ <71001778+Quuuod@users.noreply.github.com>
2021-02-07 18:55:23 +0300

    Slider + header color
    Added slider and updated header color

--- javascript/HeaderResizeWhenScrolled.js
+++ javascript/HeaderResizeWhenScrolled.js
@@ -1,12 +1,28 @@
 window.onscroll = function () {
     var top = window.scrollY;
-    var header = document.getElementsByClassName('header');
     var offset = 60;
+
+    var header = document.getElementsByClassName('header');
+    var links = document.getElementsByClassName('menu__list');
+    var line = document.getElementsByClassName('menu__line');
+    
     if (top > offset) {
         header[0].classList.remove('top');
         header[0].classList.add('scrolled');
+
+        links[0].classList.remove('links-top');
+        links[0].classList.add('links-scrolled');
+
+        line[0].classList.remove('line-top');
+        line[0].classList.add('line-scrolled');
     } else {
         header[0].classList.remove('scrolled');
         header[0].classList.add('top');
+
+        links[0].classList.remove('links-scrolled');
+        links[0].classList.add('links-top');
+
+        line[0].classList.remove('line-scrolled');
+        line[0].classList.add('line-top');
     }
 };
